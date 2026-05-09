567521c6fa7aa48d2ad568ec3c436ae12edc758a
Abdul Ali <abdulali0407@gmail.com>
2026-05-09 22:02:46 +0530

    fix reports page url

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -76,7 +76,7 @@ const email = user?.email;
 
       const res = await axios.get(
         // `http://localhost:8080/api/report/user/${email}`
-        `https://ecotrack-backend-4our.onrender.com/api/report/user/{email}`
+        `https://ecotrack-backend-4our.onrender.com/api/report/user/${email}`
       );
 
       setData(res.data);
